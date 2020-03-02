dad8aad864bf2757418f955a22b856a74d30a681
asbaden <gusbaden96@gmail.com>
2020-03-01 21:34:00 -0800

    added user table in migration file

---
+++ data/migrations/20200301212722_users.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", users => {
+    users.increments();
+
+    users.string("first_name", 128).notNullable();
+
+    users.string("last_name", 128).notNullable();
+
+    users
+      .string("email", 128)
+      .notNullable()
+      .unique();
+    users.string("password", 128).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
