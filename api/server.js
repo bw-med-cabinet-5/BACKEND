e1bfe36c79c147cc3ef32d394ed9e01f45a07198
asbaden <gusbaden96@gmail.com>
2020-03-01 17:28:19 -0800

    added server tests

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+// const authenticate = require
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use("/api/auth", authRouter)
+// server.use("/api/strains", authenticate, strainsRouter)
+
+module.exports = server 
