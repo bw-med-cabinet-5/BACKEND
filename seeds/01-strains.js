1993fdba7502775eeda809d546f1583b5a868aaa
asbaden <gusbaden96@gmail.com>
2020-03-05 14:11:01 -0800

    brought back truncate

--- seeds/01-strains.js
+++ seeds/01-strains.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('strains').truncate()
+  return knex('strains').del()
     .then(function () {
       // Inserts seed entries
       return knex('strains').insert([
