6d2feb58d5eb30b672b5ea4dc48d09831e1f00f7
asbaden <gusbaden96@gmail.com>
2020-03-05 13:32:23 -0800

    edit to strain seed

--- seeds/01-strains.js
+++ seeds/01-strains.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('strains').insert([
         { 
-          user_id:1,
+          
           strain_id: 1,
           name: "Afpak",
           race: "hybrid",
