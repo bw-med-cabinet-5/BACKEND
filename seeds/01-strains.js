244fb49155005e376d6c52c5f3eca8023c4f68b2
asbaden <gusbaden96@gmail.com>
2020-03-05 14:02:41 -0800

    added truncate to seed

--- seeds/01-strains.js
+++ seeds/01-strains.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('strains').del()
+  return knex('strains').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('strains').insert([
